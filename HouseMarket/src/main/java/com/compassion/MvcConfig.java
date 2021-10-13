32d120838a28f803e6956a81be75d701b70e2b0f
Rohit-MyRG <rohit.myrg@gmail.com>
2021-10-13 11:16:10 +0530

    MvcConfig updated,in webapp: css, js, images folder path added.

--- HouseMarket/src/main/java/com/compassion/MvcConfig.java
+++ HouseMarket/src/main/java/com/compassion/MvcConfig.java
@@ -3,6 +3,7 @@ package com.compassion;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -10,5 +11,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @ComponentScan("com.compassion")
 public class MvcConfig extends WebMvcConfigurerAdapter
 {
-
+	@Override
+	public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+	    registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+	    registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+	    registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+	}
 }
