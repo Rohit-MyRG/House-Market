8f17ee20fc874138c93bd8a049acd5db9f9a2b5e
Rohit-MyRG <rohit.myrg@gmail.com>
2021-10-17 11:57:41 +0530

    this is Just for testing some example

--- HouseMarket/src/main/java/com/compassion/AddController.java
+++ HouseMarket/src/main/java/com/compassion/AddController.java
@@ -9,11 +9,21 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.compassion.service.AddService;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage; 
+
 @Controller
 public class AddController {
 	
 	@RequestMapping("/log")
-	public String add(HttpServletRequest request, HttpServletResponse response) {
+	public void add(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		/*
 		 * int a = Integer.parseInt(request.getParameter("t1"));
 		 * 
@@ -23,6 +33,53 @@ public class AddController {
 		 * ModelAndView(); mv.setViewName("show.jsp"); mv.addObject("result", a); return
 		 * mv;
 		 */
-		return "index.jsp";
+
+		PrintWriter out = response.getWriter();
+		
+		
+		try{
+			
+            
+			String host ="smtp.gmail.com";
+			String user = "rohit.myrg@gmail.com";
+			String pass = "msrdpiqhxbonftim";
+			String to = "rohit.myrg@gmail.com";
+			String from = "rohit.myrg@gmail.com";
+			String subject = "A.M. College Library System";
+			String messageText = "746368";
+			boolean sessionDebug = false;
+
+			Properties props = System.getProperties();
+
+			props.put("mail.smtp.starttls.enable", "true");
+			props.put("mail.smtp.host", host);
+			props.put("mail.smtp.port", "587");
+			props.put("mail.smtp.auth", "true");
+			props.put("mail.smtp.starttls.required", "true");
+			//Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+	            
+			Session mailSession = Session.getDefaultInstance(props, null);
+			mailSession.setDebug(sessionDebug);
+			Message msg = new MimeMessage(mailSession);
+			msg.setFrom(new InternetAddress(from));
+			InternetAddress[] address = {new InternetAddress(to)};
+			msg.setRecipients(Message.RecipientType.TO, address);
+			msg.setSubject(subject); 
+			msg.setSentDate(new Date());
+			msg.setText(messageText);
+
+			Transport transport=mailSession.getTransport("smtp");
+			transport.connect(host, user, pass);
+			transport.sendMessage(msg, msg.getAllRecipients());
+			transport.close();
+	           
+			out.print("Done");
+		}catch(Exception ex)
+		{    
+			//System.out.println("OTP Sending error,Please check 'internet Connection' or incurrect Email ID :\n"+ex); 
+			out.print("Sorry");
+		}
+		
+		
 	}
 }
