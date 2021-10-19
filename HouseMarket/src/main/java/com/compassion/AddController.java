db3432a6baa445e9f83c5487532bcb77fa801c7f
Rohit-MyRG <rohit.myrg@gmail.com>
2021-10-19 11:38:17 +0530

    ignore

--- HouseMarket/src/main/java/com/compassion/AddController.java
+++ HouseMarket/src/main/java/com/compassion/AddController.java
@@ -22,7 +22,7 @@ import javax.mail.internet.MimeMessage;
 @Controller
 public class AddController {
 	
-	@RequestMapping("/log")
+	@RequestMapping("rohit")
 	public void add(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		/*
 		 * int a = Integer.parseInt(request.getParameter("t1"));
@@ -36,50 +36,6 @@ public class AddController {
 
 		PrintWriter out = response.getWriter();
 		
-		
-		try{
-			
-            
-			String host ="smtp.gmail.com";
-			String user = "rohit.myrg@gmail.com";
-			String pass = "msrdpiqhxbonftim";
-			String to = "rohit.myrg@gmail.com";
-			String from = "rohit.myrg@gmail.com";
-			String subject = "A.M. College Library System";
-			String messageText = "746368";
-			boolean sessionDebug = false;
-
-			Properties props = System.getProperties();
-
-			props.put("mail.smtp.starttls.enable", "true");
-			props.put("mail.smtp.host", host);
-			props.put("mail.smtp.port", "587");
-			props.put("mail.smtp.auth", "true");
-			props.put("mail.smtp.starttls.required", "true");
-			//Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
-	            
-			Session mailSession = Session.getDefaultInstance(props, null);
-			mailSession.setDebug(sessionDebug);
-			Message msg = new MimeMessage(mailSession);
-			msg.setFrom(new InternetAddress(from));
-			InternetAddress[] address = {new InternetAddress(to)};
-			msg.setRecipients(Message.RecipientType.TO, address);
-			msg.setSubject(subject); 
-			msg.setSentDate(new Date());
-			msg.setText(messageText);
-
-			Transport transport=mailSession.getTransport("smtp");
-			transport.connect(host, user, pass);
-			transport.sendMessage(msg, msg.getAllRecipients());
-			transport.close();
-	           
-			out.print("Done");
-		}catch(Exception ex)
-		{    
-			//System.out.println("OTP Sending error,Please check 'internet Connection' or incurrect Email ID :\n"+ex); 
-			out.print("Sorry");
-		}
-		
-		
+		out.print("Hello Rohit");
 	}
 }
