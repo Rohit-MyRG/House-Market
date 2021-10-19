78dc3fd26f2e5e9c12e50cd9826005073df6bfe4
Rohit-MyRG <rohit.myrg@gmail.com>
2021-10-19 11:40:33 +0530

    MySQl username and password.

---
+++ HouseMarket/src/main/java/com/compassion/dao/MySqlProvider.java
@@ -0,0 +1,8 @@
+package com.compassion.dao;
+
+public interface MySqlProvider {
+	String DRIVER="com.mysql.cj.jdbc.Driver";  
+	String CONNECTION_URL="jdbc:mysql://localhost:3306/rk_housemarket";  
+	String USERNAME="root";  
+	String PASSWORD="MySQL@2021";  
+}
